380d81f36968576e26b7a12e9eda1ad98a353367
shimiwaka <kei_ikeda@r.recruit.co.jp>
2023-11-25 19:09:16 +0900

    create template

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type GameState int
+
+type Game struct {
+	State GameState
+}
+
+const (
+	gameTitle = "ヒーローガールスカイパンチ"
+	screenX   = 500
+	screenY   = 800
+)
+
+func NewGame() *Game {
+	g := &Game{State: 0}
+	return g
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return screenX, screenY
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	ebiten.SetWindowSize(screenX, screenY)
+	ebiten.SetWindowTitle(gameTitle)
+	if err := ebiten.RunGame(NewGame()); err != nil {
+		log.Fatal(err)
+	}
 }
